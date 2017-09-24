9dcc356e7ad0b9c950cfc14d5f147439417565b1
饒義忠 <kanneg881@gmail.com>
2017-09-24 20:26:02 +0800

    Update custom_error.php
    新增錯誤顯示為段落
    新增示範自訂錯誤訊息

--- error and exception/custom_error.php
+++ error and exception/custom_error.php
@@ -16,6 +16,7 @@
  */
 function customError($error_level, $error_message, $error_file, $error_line)
 {
+    echo "<p>";
     //顯示錯誤等級
     switch ($error_level) {
         // 執行時警告
@@ -48,7 +49,7 @@ function customError($error_level, $error_message, $error_file, $error_line)
             echo "<b>未知的錯誤:</b>";
             break;
     }
-    echo "[$error_level] $error_message<br>檔案位置：$error_file 第 $error_line 行<br><br>";
+    echo "[$error_level] $error_message<br>檔案位置：$error_file 第 $error_line 行</p>";
 }
 
 // 設定自定義錯誤函數處理
@@ -59,4 +60,11 @@ echo $undefined;
 
 // 錯誤的除法，除數不能為0
 echo 1 / 0;
+
+// 示範自訂錯誤訊息
+$one = 2;
+if ($one !== 1) {
+    // 觸發錯誤
+    trigger_error('$one 變數要設為1才合理', E_USER_NOTICE);
+}
 ?>
